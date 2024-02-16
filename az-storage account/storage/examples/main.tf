630172e52984068e814ed4b6d062a92a4648d7d2
Muhammed Ibrahim <muhammedibrahim@Muhammeds-MacBook-Pro.local>
2024-02-16 14:37:14 -0600

    tested storage account module

--- az-storage account/storage/examples/main.tf
+++ az-storage account/storage/examples/main.tf
@@ -16,11 +16,11 @@ module "storage_account" {
     module_config = {
       project = "cloudops"
       location = "southcentralus"
-      resource_group_name = "ibrahim-demoRG"
+      resource_group_name = "DemoRG-1"
     # virtual_network = {
     #   virtual_network_subnet_ids = ["/subscriptions/306eb98e-26d5-4606-b5bc-c57350db64b8/resourceGroups/NetworkRG/providers/Microsoft.Network/virtualNetworks/vnet-platformdep-nonprod-sc01"]
     # }
-    environment = "dev"
+    environment = "mydevenv01"
     additional_tags = {
       "Owner"      = "EnterpriseCloudOps@bnsf.com"
       "CostCenter" = "80219"
