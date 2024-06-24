eb0d078815b9ec78fdb3400f03011785b51aa701
muhammed-ai <134043418+muhammed-ai@users.noreply.github.com>
2024-06-24 01:18:02 -0500

    Delete virtual-network directory

--- virtual-network/data.tf
+++
@@ -1,6 +0,0 @@
-# Get the values from the data source
-data "azurerm_private_dns_resolver_dns_forwarding_ruleset" "dns_ruleset" {
-  provider            = azurerm.dns
-  name                = local.dns_resolver_forwarding_ruleset.name
-  resource_group_name = local.dns_resolver_forwarding_ruleset.resource_group_name
-}
